ac333952dd46a26fb23ec89b994f3f5021093174
Louis Northmore <louis.northmore@gmail.com>
2017-04-24 12:50:42 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,42 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import lxml.html
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+
+#functions
+
+def getHTMLvalue(el, css, atr='text', index=0):
+    try:
+        lst = el.cssselect(css)
+        val = ''
+        if lst != None and len(lst) > 0:
+            if index==0 or len(lst) >= index +1:
+                if atr=='text':
+                    val = lst[index].text_content()
+                else:
+                    val = lst[index].attrib.get(atr)
+    
+        return val
+    except:
+        return ''
+
+baseurl = 'http://www.just-eat.co.uk'
+postcodes = ['pr1','wn5','bb2']
+
+for postcode in postcodes:
+    url = 'http://www.just-eat.co.uk/area/' + postcode
+    html = scraperwiki.scrape(url)
+    root = lxml.html.fromstring(html)
+    for restaurant in root.cssselect(".restaurantWithLogo"):
+        record = {}
+        record['name'] = getHTMLvalue(restaurant, "h3")
+        link = getHTMLvalue(restaurant, "h3 a", "href")
+        record['link'] = link
+        record['url'] = baseurl + link
+        record['address'] = getHTMLvalue(restaurant, "address")
+        record['foodtype'] = getHTMLvalue(restaurant, '.restaurantCuisines').replace("Type of food", "")
+        
+        #get BT details
+
+        scraperwiki.sqlite.save(['link'], record)
+        #print name, link, address, foodtype
